e5e1ec9b056bb1900fd06080ad59d31b196e2c41
SD05 <oneiroi@outlook.com>
2012-12-06 06:43:22 +0900

    2 Chars

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,34 +1,11 @@
 var EXCUTE = function() {
 	var value = CryptoJS.MD5($("#userInputField").val()).toString();
-	var key   = $("span.key");
-	var keys  = "" + key.eq(0).html() + key.eq(1).html() + key.eq(2).html();
-	md5view(value, document.getElementById("MD5ViewResult"), keys)
+	md5view(value, document.getElementById("MD5ViewResult"))
 	$("#md5ResultHex").html(value);
 }
 $(document).ready(function(){
 	SyntaxHighlighter.all();
 	EXCUTE();
-	$("#excute").click(EXCUTE);
-	$("#userInputField").focus(function(){
-		$("span.key").removeClass("focus");
-	});
 	$("#userInputField").keyup(EXCUTE);
-	
-	$("body").click(function(){
-		$("span.key").removeClass("focus");
-	});
-
-	$("span.key").click(function(e) { 
-		e.stopImmediatePropagation();
-		$("span.key").removeClass("focus");
-		$(this).addClass("focus");
-	});
-	$("body").keypress(function(e){
-		var check = $("span.key.focus");
-		if (check.length) {
-			check.html(String.fromCharCode(e.charCode));
-			EXCUTE();
-		}
-	});
 	$("#maker").html("oneiroi@outlook.com");
 });
--- md5view.js
+++ md5view.js
@@ -1,18 +1,16 @@
-var md5view = function(value, element, key) {
+var md5view = function(value, element) {
 	var background = [
 		value.substring(0,6), 
 		value.substring(6,12), 
 		value.substring(12,18), 
 		value.substring(18,24), 
-		value.substring(24,30), 
-		value.charAt(30), 
-		value.charAt(31)
+		value.substring(24,30)
 	];
-	var key = [value.charAt(30), value.charAt(31), key&&key.charAt(0)||"M", key&&key.charAt(1)||"D", key&&key.charAt(2)||"5"];
-	var html = '<div class="md5View">\n<ul class="md5ViewList">\n';
-	for (var i=0; i < 5;i++) {
-		html += '<li class="md5ViewValue" style="background:#'+background[i]+'">' +key[i]+'</li>\n';
-	}
+	var html = '<div class="md5View" style="border-color:#'+background[0]+'">\n<ul class="md5ViewList">\n';
+	html += '<li class="md5ViewValue" style="background:#'+background[1]+'">';
+	html += '<span class="overlay" style="color:#'+background[3]+'">' +value.charAt(30)+'</span><span>' +value.charAt(30)+'</span>';
+	html += '</li>\n<li class="md5ViewValue" style="background:#'+background[2]+'">';
+	html += '<span class="overlay" style="color:#'+background[4]+'">' +value.charAt(31)+'</span><span>' +value.charAt(31)+'</span></li>\n';
 	html += '</ul>\n<div style="clear:both"></div>\n</div>';
 	if (element) {
 		element.innerHTML = html;
